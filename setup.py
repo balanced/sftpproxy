db2607cbc9afb7a3743f096fb07d4f661026a7d2
Victor Lin <bornstub@gmail.com>
2014-11-18 13:31:24 -0800

    Init project

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+tests_require = [
+    'nose',
+    'nose-cov',
+    'mock',
+]
+
+version = '0.0.0'
+try:
+    import sftpproxy
+    version = sftpproxy.__version__
+except ImportError:
+    pass
+
+setup(
+    name='sftpproxy',
+    version=version,
+    packages=find_packages(),
+    url='https://github.com/balanced/sftpproxy',
+    author='victorlin',
+    author_email='victorlin@balancedpayments.com',
+    install_requires=[
+
+    ],
+    extras_require=dict(
+        tests=tests_require,
+    ),
+    tests_require=tests_require,
+    test_suite='nose.collector',
+    entry_points="""\
+    """
+)
